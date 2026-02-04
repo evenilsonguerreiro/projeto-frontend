352c5c3d656c5db5b0a933ae6a86d543d4b72f60
evenilson <evenilsonguerreiro@gmail.com>
2026-02-04 18:58:58 -0300

    ajustes moblie

--- src/components/OfertaSpecial/style.js
+++ src/components/OfertaSpecial/style.js
@@ -6,53 +6,39 @@ export const ContainerOferta = styled.div`
     display: flex;
     justify-content: center;
     background-color: white;
-   
-   
-   
-`
-   
+`;
+
 export const ConteudoOferta = styled.div`
     max-width: 1200px;
     width: 100%;
     display: flex;
     align-items: center;
     padding: 60px 20px;
-    gap: 10px;
+    gap: 60px;
     margin: 0 auto;
     background-color: white;
-   
-   
-   
 
-
-    @media (max-width:768px) {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-       
-
-        
-    }
-   
-
+    
     h2 {
-        font-size: 28px;
-        font-weight:bold;
-        line-height: 36px;
+        font-size: 48px; 
+        font-weight: bold;
+        line-height: 50px;
         letter-spacing: 1px;
+        color: ${Cores.darkGray};
     }
+
     span {
         font-size: 14px;
         color: ${Cores.primary};
         font-weight: bold;
         line-height: 22px;
     }
+
     p {
-        font-size: 14px;
-        line-height: 22px;
+        font-size: 16px;
+        line-height: 28px;
         color: ${Cores.lightGray};
+        margin-bottom: 10px;
     }
 
     .imgOferta {
@@ -71,22 +57,56 @@ export const ConteudoOferta = styled.div`
         flex-direction: column;
         justify-content: center;
         gap: 20px;
+        align-items: flex-start; 
     }
 
     button {
         width: 200px;
-        height: 40px;
-        font-size: 14px;
+        height: 48px;
+        font-size: 16px;
         font-weight: bold;
         border-radius: 8px;
-        background-color:${Cores.primary};
+        background-color: ${Cores.primary};
         border: none;
         color: ${Cores.white};
         line-height: 22px;
         cursor: pointer;
 
         &:hover {
-             background-color: #ab1b60;
+            background-color: #ab1b60;
         }
     }
-`
+
+  
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: flex-start; 
+        padding: 40px 20px;
+        gap: 20px;
+
+        h2 {
+            font-size: 28px; 
+            line-height: 36px;
+        }
+
+        p {
+            font-size: 14px;
+            line-height: 22px;
+        }
+
+        .imgOferta {
+            width: 100%;
+            max-width: 100%;
+        }
+
+        .informacoes-oferta {
+            width: 100%;
+            align-items: flex-start; 
+        }
+
+        button {
+            width: 200px;
+            height: 40px;
+        }
+    }
+`;
